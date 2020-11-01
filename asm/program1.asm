27d680689bb11659378d13b4e4d4fb581eb74385
mstites <maeve@mstites.com>
2020-10-31 20:06:22 -0400

    Fix bad register calls

--- asm/program1.asm
+++ asm/program1.asm
@@ -2,48 +2,48 @@
 # using loops in this version
 # originally tried to make it find highest and lowest but that was too hard :(
 
-addi $a0, $zero, 8      # input value 0
+addi $a0, $zero, 82      # input value 0
 addi $a1, $zero, 4 	# input value 1
-addi $a2, $zero, 3	# input value 2 
-addi $a3, $zero, 3	# input value 3 
+addi $a2, $zero, 200	# input value 2 
+addi $a3, $zero, 137	# input value 3 
 
-addi $t0, $zero, 1 	# loop counter
+addi $t0, $zero, 1 	# n
 addi $t1, $a0, 0 	# current compare 1
 addi $t2, $a1, 0 	# current compare 2
-addi $t3, $zero, 0	# highest value
+
+addi $s0, $zero, 0	# highest value
 
 loop:
 	addi $t0,$t0,1        #n++
-	slt $t3, $t0, $t1 	# find the lowest of the two values
-	bne $t3, $zero, firstMore # if t0 is greater
-	beq $t3, $zero, secondMore # if t1 is greater
+	slt $s0, $t1, $t2 	# find index of the highest value
+	beq $s0, $zero, firstMore # if t0 is greater
+	bne $s0, $zero, secondMore # if t1 is greater
+	
+firstMore:
+	addi $s0, $t1, 0 # set highest to t1
+	j update
 
-update: 
-	# update the values for comparison
+secondMore:
+	addi $s0, $t2, 0 # set highest to t2
+	j update
+
+update:  		# update values for next comparison based on n
 	beq $t0, 2, compare2
 	beq $t0, 3, compare3
 	beq $t0, 4, finish
 		
 compare2:
-	addi $t1, $t3, 0 # last highest
-	addi $t2, $a2, 0 # new value
+	addi $t1, $s0, 0 # last highest
+	addi $t2, $a2, 0 # value 3
 	j loop
 
 compare3: 
-	addi $t1, $t3, 0 # last highest
-	addi $t2, $a3, 0 # new value
+	addi $t1, $s0, 0 # last highest
+	addi $t2, $a3, 0 # value 4
 	j loop
 	
-firstMore:
-	addi $t3, $a0, 0 # set highest
-	j update
-
-secondMore:
-	addi $t3, $a1, 0 # set highest
-	j update
-
 finish: 
-	add $t3,$zero,$t3     #return the answer
+	add $s0,$zero,$s0     #return the answer
 	addi $v0,$zero,1      #set syscall type to print int
 	SYSCALL               #print $a0
 	addi $v0,$zero,10     #set syscall type to exit 
