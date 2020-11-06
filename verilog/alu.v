93a6d623582b4afcd387691f303441bdf4cb6ecc
mstites <maeve@mstites.com>
2020-11-05 20:51:59 -0500

    Add already made alu code

--- verilog/alu.v
+++ verilog/alu.v
@@ -17,3 +17,48 @@ module ALU
   end
 
 endmodule
+
+# `include "defines.v"
+# `timescale 1ns / 1ps
+# 
+# module ALU
+#  #(parameter W = 4,
+#            DLY = 5)
+#   (input     [2:0] Ctrl,// Control word to select operation
+#    input      [W-1:0] A,// Input Operand
+#    input      [W-1:0] B,// Output Operand
+#    output reg [W-1:0] R,// Result
+#    output reg      cout,// Was there a carry out? Unsigned overflow
+#    output reg overflow);// Result overflowed
+# 
+#   wire [W:0]   carry;
+#   wire [W-1:0] result;
+# 
+#   reg subtraction; // for two's complement
+# 
+#   always 0* begin
+#     case (Ctrl) // because Ctrl determines what we are doing
+#       'ADD_: begin subtraction = 1'b0; end
+#       'SUB_: begin subtraction = 1'b1; end // make sure it's signed for 2's
+#       default: begin subtraction = 1'b0; end
+#     endcase
+#   end
+# 
+#   assin carry[0] = subtraction; // so that we add a 1 or not add a 1
+# 
+#   generate genvar i;
+#     for (i=0;i<W;i=i+1) begin
+#       ALU_SLICE #(.DLY(DLY)) slice_inst(Ctrl,A[i],B[i],carry[i],result[i],carry[i+1]);
+#     end
+#   endgenerate
+# 
+#   // checking if there is an overflow (we check the last two carrys)
+#   xor #DLY xoroverflow(overflow, carry[W-1], carry[W]);
+# 
+#   // setting cout value to carry
+#   assign cout = carry;
+# 
+#   // setting R value to result
+#   assign R = result;
+# 
+# endmodule
